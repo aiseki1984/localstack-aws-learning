d015cbc5f755d1340e04faff0388fe31dcf96d18
aiseki1984 <a.iseki1984@gmail.com>
2025-11-16 17:58:20 +0900

    setup cdk project

---
+++ playground/project-03-cdk-02/test/project-03-cdk-02.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Project03Cdk02 from '../lib/project-03-cdk-02-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/project-03-cdk-02-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Project03Cdk02.Project03Cdk02Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
