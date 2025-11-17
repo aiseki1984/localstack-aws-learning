aa46306ea526fa05af08ba77b1726cfd0be2e6e9
aiseki1984 <a.iseki1984@gmail.com>
2025-11-17 21:05:21 +0900

    setup project

---
+++ playground/project-04-frontend-app-01/test/project-04-frontend-app-01.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Project04FrontendApp01 from '../lib/project-04-frontend-app-01-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/project-04-frontend-app-01-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Project04FrontendApp01.Project04FrontendApp01Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
